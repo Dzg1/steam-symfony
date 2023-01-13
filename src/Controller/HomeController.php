151a1beaa2a6bc7db40ca79b2973e2b5e8300e8b
DzG <ifellous01@gmail.com>
2023-01-13 15:15:13 +0100

    security in progress

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
+    
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
